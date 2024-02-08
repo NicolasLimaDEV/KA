300d678f0cf8456fc411863f1eeab91eecb7d662
Nicolas Lima <nicolaslima.dev.web@gmail.com>
2024-02-07 21:48:20 -0300

    subindo projeto teste

---
+++ script.js
@@ -0,0 +1,5 @@
+const btnMobile = document.querySelector('.btn-mobile')
+btnMobile.addEventListener('click', ()=> {
+  const menu = document.querySelector('.menu')
+  menu.classList.toggle('active')
+})
